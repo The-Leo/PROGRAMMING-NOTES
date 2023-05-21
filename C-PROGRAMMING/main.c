d299a24140b7ade677d467191d6f9492048545a5
Leonard Odoi <odoi.leonard1@gmail.com>
2023-05-21 10:44:36 +0000

    Add C programming notes

---
+++ C-PROGRAMMING/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+    char characterName [] = "Leo";
+    int charAge = 31;
+
+    printf("There was a young man named %s\n", characterName);
+    printf("He is %d years grown\n", charAge);
+    printf("  /  |\n");
+    printf(" /   |\n");
+    printf("/____|\n");
+
+
+    printf("%lf\n", 5.0);
+
+    printf("%f", 5.0/4);
+    return 0;
+}
+
