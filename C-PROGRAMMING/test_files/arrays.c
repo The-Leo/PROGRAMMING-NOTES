c42d709c0598d7a762457c3df9bf1982c73aa8ec
Leonard Odoi <odoi.leonard1@gmail.com>
2023-06-27 16:50:13 +0000

    Another example of arrays

--- C-PROGRAMMING/test_files/arrays.c
+++ C-PROGRAMMING/test_files/arrays.c
@@ -3,16 +3,16 @@
 
 int main(void){ //main function -Entry point
 
-int i, j;
-int n[5]; //array declaration
+int n[5] = {1, 2, 3, 4, 5};
 
-for(i=0; i<5; i++){
-    n[i]=i+100; //Array initialization
-}
+int numbers[5];
+printf("Please enter 3 numbers\n");
+scanf("%d", &numbers[0]);
+scanf("%d", &numbers[1]);
+scanf("%d", &numbers[2]);
 
-for(j=0; j<5; j++){
-    printf("Element[%d] = %d\n" ,j ,n[j]); //Accessing the array
-}
-
+printf("The value in index [0] is %d\n", numbers[0]);
+printf("The value in index [1] is %d\n", numbers[1]);
+printf("The value in index [2] is %d\n", numbers[2]);
     return 0;
 }
